979e3b7260732fd8e9f67589ad09b42165371a6b
Alexander Buyanov <alex.bu.89@gmail.com>
2018-03-01 23:00:28 +0100

    add sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Select from './components/select/Select';
+import Select from './components/select/select';
+import './app.scss';
 
 const App = () => {
   return <Select />;
--- webpack.config.js
+++ webpack.config.js
@@ -18,20 +18,14 @@ module.exports = {
         use: ['babel-loader']
       },
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              camelCase: true,
-              sourceMap: true
-            }
-          }
-        ]
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
       }
     ]
   },
