a10c34b5ae1a50da264650d687f4c4c6b074eb73
alex-bu-89 <alex.bu.89@gmail.com>
2018-03-05 13:02:19 +0100

    fix formatting

--- src/components/select/Select.js
+++ src/components/select/Select.js
@@ -2,60 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Select extends React.Component {
-  constructor (props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
     this.handleChange = this.handleChange.bind(this);
     this.handleMouseDown = this.handleMouseDown.bind(this);
 
     this.state = {
-			value: this.props.value || '',
-			options: this.props.options,
-			isOpen: false,
+      value: this.props.value || '',
+      options: this.props.options,
+      isOpen: false,
       isSearchable: false
-		};
+    };
   }
 
-  handleChange (e) {
+  handleChange(e) {
     e.stopPropagation();
-		e.preventDefault();
-    this.setState({ value: e.target.value });
+    e.preventDefault();
+    this.setState({value: e.target.value});
   }
 
-  handleMouseDown (e) {
+  handleMouseDown(e) {
     e.stopPropagation();
-		e.preventDefault();
-    this.setState({ isOpen: !this.state.isOpen });
+    e.preventDefault();
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
 
   renderDropdown() {
     if (this.state.isOpen) {
-      return (
-        <ul className="select-box--options">
-          {
-            this.state.options.map((option, i) => {
-              return <li key={i}>{ option }</li>
-            })
-          }
-        </ul>
-      )
+      return (<ul className="select-box--options">
+        {
+          this.state.options.map((option, i) => {
+            return <li key={i}>{option}</li>
+          })
+        }
+      </ul>)
     }
   }
 
   render() {
-    return (
-      <section className='select-box'>
-        <input
-          type="text"
-          name="select-box"
-          onChange={(e) => { this.handleChange }}
-          onMouseDown={this.handleMouseDown}
-          value={ this.state.value }
-          readOnly={ !this.state.isSearchable } />
-
-        { this.renderDropdown() }
-      </section>
-    );
+    return (<section className='select-box'>
+      <input type="text" name="select-box" onChange={(e) => {
+          this.handleChange
+        }} onMouseDown={this.handleMouseDown} value={this.state.value} readOnly={!this.state.isSearchable}/> {this.renderDropdown()}
+    </section>);
   }
 }
 
