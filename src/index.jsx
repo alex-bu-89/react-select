8e37e88877c4471e4a3f09b0e0b864a2a19d83a3
alex-bu-89 <alex.bu.89@gmail.com>
2018-03-20 15:42:20 +0100

    add scroll for dropdown

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ ReactDOM.render(
   (
     <Select
       value="start value"
-      options={['options 1', 'options 2', 'options 3']}
+      options={['options 1', 'options 2', 'options 3', 'optionsssssssssssssssssssssss 4', 'options 5', 'options 6']}
     />
   ),
   document.getElementById('root'),
