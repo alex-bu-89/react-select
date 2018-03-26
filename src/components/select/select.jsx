6a8d3ef3130496ba66ddcaa627be84288c1aed6b
alex-bu-89 <alex.bu.89@gmail.com>
2018-03-26 13:01:42 +0200

    add semi...

--- src/components/select/select.jsx
+++ src/components/select/select.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import * as FontAwesome from 'react-icons/lib/fa'
+import * as FontAwesome from 'react-icons/lib/fa';
 
 class Select extends React.Component {
   constructor(props) {
